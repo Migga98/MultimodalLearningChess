9f3760a8b13f4573cb21a3004ed07f191bc1c53e
Mika Pommeranz <64205512+Migga98@users.noreply.github.com>
2022-08-31 23:13:28 +0200

    setup

--- src/train_vit.py
+++ src/train_vit.py
@@ -54,7 +54,7 @@ notebook_login()
 if __name__ == "__main__":
     experiment_name = 'ViT_Chess_DGX_V9'
     batch_size = 64  # 256
-    epochs = 15
+    epochs = 20
     learning_rate = 2e-5
     warmup_steps = 1e2
     epsilon = 1e-8
