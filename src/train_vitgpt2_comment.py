8240e443fc70acb6adb242c96901bd4535185f8e
Mika Pommeranz <64205512+Migga98@users.noreply.github.com>
2022-09-03 09:45:17 +0200

    setup

--- src/train_vitgpt2_comment.py
+++ src/train_vitgpt2_comment.py
@@ -55,7 +55,7 @@ def format_time(elapsed):
 if __name__ == "__main__":
     experiment_name = 'ViTGPT_Chess_comment_DGX_V1'
     batch_size = 32
-    epochs = 2
+    epochs = 8
     learning_rate = 2e-5
     warmup_steps = 1e2
     epsilon = 1e-8
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     state_data = []
     comment_data = []
     length = data.shape[0]
-    for i in range(int(length * 0.1)):
+    for i in range(int(length * 1)):
         state = data[:][i][0]
         comment = data[:][i][2]
         state_data.append(state)
