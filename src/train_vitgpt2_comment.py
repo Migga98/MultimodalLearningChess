0e16a4dd1dd9f0099cd5b0f5bf80ed16e3b64fbb
Mika Pommeranz <64205512+Migga98@users.noreply.github.com>
2022-09-03 14:19:49 +0200

    setup

--- src/train_vitgpt2_comment.py
+++ src/train_vitgpt2_comment.py
@@ -53,7 +53,7 @@ def format_time(elapsed):
 
 
 if __name__ == "__main__":
-    experiment_name = 'ViTGPT_Chess_comment_DGX_V2'
+    experiment_name = 'ViTGPT_Chess_comment_DGX_V3'
     batch_size = 32
     epochs = 8
     learning_rate = 2e-5
